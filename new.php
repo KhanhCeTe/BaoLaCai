e8133975e4e7177c2915d43735623311d5ec1845
unknown <phambaokhanh254@gmail.com>
2022-07-04 09:01:51 +0700

    Release

--- new.php
+++ new.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Báo Ná Cải</title>
-	<meta name="robots" content="noindex">
+	<title>Báo ná cải</title>
 	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 	<link rel="shortcut icon" href="img/icon1.jpg">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -12,28 +11,28 @@
 	<link rel="stylesheet" href="https://raw.githubusercontent.com/daneden/animate.css/master/animate.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
 	 crossorigin="anonymous">
-	<link rel="stylesheet" href="css/style2.css">
-	<link href="img">
-	<script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="css/style3.css">
+	<link rel="new.php">
 </head>
 <body>
- 	<nav>
- 		<input type="checkbox" id="check" >
- 		<label for="check" class="checkbtn">
- 			<i class="fas fa-bars"></i> 
- 		</label>
- 		<label class="logo">BAONACAI</label>
- 		 <ul>
- 		 	<li><a href="index.html" class="logo">Trang chủ</a></li>
- 		 	<li><a href="thongtin.html">Thông tin</a></li>
- 		 	<li><a href="index.html">Ảnh</a></li>
- 		 	<li><a href="index.html">Liên hệ</a></li>
- 		 	<li><a href="news.html">Tin tức</a></li>
- 		 </ul>
+	<form method="post" action="">
+	  <div class="login">
+		<h2>Login</h2>
+		<label class="user">Username</label>
+		<input type="text"></br>
+		<label>Pass</label>
+		<input type="password" >
+		<button type="submit" name="login">Login</button>
+
+		
+	  </div>
+	</form>
+	  <?php 
+	  	if(isset($POST['login'])){
+			header('location:index.html');
+		  }
+	  ?>
 
 
-
- 	</nav>	
- 	<section></section>
 </body>
 </html>
